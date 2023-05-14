f82cf9b4ab1cab1001a993b765a869bfc63d9614
Иван Данилов <6350654@gmail.com>
2023-05-15 01:17:23 +0300

    Finall

---
+++ Reverse Polish Notation/Reverse Polish Notation/FileReader.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace Reverse_Polish_Notation
+{
+    class FileReader
+    {
+        public static string Read()
+        {
+            string fileName = "File.txt";
+            string line = string.Empty;
+            try
+            {
+                using (StreamReader sr = new StreamReader(fileName))
+                {
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        return line;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error reading file: " + e.Message);
+            }
+            return line;
+        }
+    }
+}
--- Reverse Polish Notation/Reverse Polish Notation/Main.cs
+++ Reverse Polish Notation/Reverse Polish Notation/Main.cs
@@ -15,32 +15,31 @@ namespace Reverse_Polish_Notation
     {
         public static int Main(string[] args)//создание основного консольного меню программы с выбором действий
         {
-            int menu = 0;
-            string expression = string.Empty;
-            while (menu!=3)
+
+            string menu = string.Empty;
+            while (menu!="3")
             {
                 Console.Clear();
                 Console.WriteLine("Выберете один из следующих вариантов: \n 1. Обратной польская запись через стэк  \n 2. Обратнаая польская запись деревом \n 3. Закрыть программу");
-                menu = int.Parse(Console.ReadLine());
+                menu = Console.ReadLine();
                 switch (menu)
                 {
-                    case 1:
+                    case "1":
                         Console.Clear();
-                        Console.Write("Введите выражение:");
-                        expression = Console.ReadLine();
-                        Console.WriteLine(AlgorithmStack.GetExpression(expression));
+                        if (FileReader.Read() != null) Console.WriteLine(AlgorithmStack.GetExpression(FileReader.Read()));
+                        else Console.WriteLine("Файл пустой");
                         Console.WriteLine("Нажмите клавиши, чтобы продолжить...");
                         Console.ReadKey();
                         break;
-                    case 2:
+                    case "2":
                         Console.Clear();
                         Console.Write("Введите выражение:");
-                        expression = Console.ReadLine();
-                        Console.WriteLine(AlgorithmTree.ConvertToReversePolishNotationString(expression));
+                        if (FileReader.Read() != null) Console.WriteLine(AlgorithmTree.ConvertToReversePolishNotationString(FileReader.Read()));
+                        else Console.WriteLine("Файл пустой");
                         Console.WriteLine("Нажмите клавиши, чтобы продолжить...");
                         Console.ReadKey();
                         break;
-                    case 3:
+                    case "3":
                         break;
                     default:
                         Console.Write("Неправильный ввод");
